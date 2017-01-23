1b6af8b71acc1ffad128bd8e98dcd0ab859e22df
Tomas van der Wansem <tomas@tomasvdw.nl>
2017-01-23 11:40:43 +0100

    Add large file tests

---
+++ tests/test_file1.rs
@@ -0,0 +1,83 @@
+
+extern crate bitcrust_lib;
+
+extern crate byteorder;
+
+
+use std::io::BufReader;
+use std::fs::File;
+
+mod blk_file;
+
+
+
+#[test]
+fn load_file1() {
+
+    let mut store = bitcrust_lib::init();
+
+    let fileno = 0;
+    let name = format!("./data/blk{:05}.dat", fileno);
+    println!("Processing {}", name);
+    let f = File::open(name).unwrap();
+    let mut rdr = BufReader::new(f);
+
+    let mut blocks = 0;
+    loop {
+        let blk = blk_file::read_block(&mut rdr).unwrap();
+
+        if blk.is_none() {
+            break;
+        }
+
+        bitcrust_lib::add_block(&mut store, &blk.unwrap());
+
+        blocks += 1;
+
+
+        if blocks == 2 {
+            break;
+        }
+
+    }
+
+
+
+}
+
+
+
+
+
+#[test]
+#[ignore]
+fn load_file_large() {
+
+    let mut store = bitcrust_lib::init();
+    let mut blocks = 0;
+
+    for fileno in 0..99 {
+        let name = format!("./data/blk{:05}.dat", fileno);
+        println!("Processing {}", name);
+        let f = File::open(name).unwrap();
+        let mut rdr = BufReader::new(f);
+
+        loop {
+            let blk = blk_file::read_block(&mut rdr).unwrap();
+
+            if blk.is_none() {
+                break;
+            }
+
+            bitcrust_lib::add_block(&mut store, &blk.unwrap());
+
+            blocks += 1;
+
+
+            if blocks == 300000 {
+                break;
+            }
+        }
+    }
+
+}
