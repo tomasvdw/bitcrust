5387817fda5ff5a37260b1b5e38e6a8414cc6435
Tomas van der Wansem <tomas@tomasvdw.nl>
2017-03-16 09:36:45 +0100

    Ignore leasing zeros when importing blk-files
    There seem to be occasions where the magic number is preceded
    by some zeros; for now we ignore.

--- tests/blk_file.rs
+++ tests/blk_file.rs
@@ -18,19 +18,30 @@ const MAGIC: u32 = 0xD9B4BEF9;
 /// bitcoin-core and various other implementations
 pub fn read_block(rdr: &mut io::Read) -> Result<Option<Vec<u8>>, io::Error> {
 
-    let magicnr    = rdr.read_u32::<LittleEndian>();
-    if magicnr.is_err() {
-        return Ok(None)
-    }
+    loop {
+        let magicnr = rdr.read_u32::<LittleEndian>();
+        match magicnr {
+            Err(_)     => return Ok(None), // assume EOF
+            Ok(m) => match m {
 
+                // TODO investigate.
+                // this happens on bitcrust-1 at blcok  451327
+                // file blk000760, file pos 54391594
+                // first 8 zero-bytes before magicnr
+                // for now we skip them
+                0     => continue,
+
+                MAGIC => break,
+                _     =>return Err(io::Error::new(io::ErrorKind::InvalidData, "Incorrect magic number"))
+            }
+        }
+
+    }
 
 
     let length     = try!(rdr.read_u32::<LittleEndian>());
     let mut buffer = vec![0; length as usize];
 
-    if magicnr.unwrap() != MAGIC {
-        return Err(io::Error::new(io::ErrorKind::InvalidData, "Incorrect magic number"));
-    }
 
     try!(rdr.read_exact(&mut buffer));
 
