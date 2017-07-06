208d09e3fbca73e2a015b9dc2989563c39427f5f
Chris MacNaughton <chris@centaurisolutions.nl>
2017-07-06 09:11:58 +0200

    initial client implementation

---
+++ bitcrustd/src/peer.rs
@@ -0,0 +1,276 @@
+use std::collections::HashSet;
+use std::fmt::{self, Debug};
+use std::io::Error;
+use std::thread;
+use std::time::{Duration, Instant};
+use std::net::Ipv6Addr;
+use std::str::FromStr;
+use std::time::{UNIX_EPOCH, SystemTime};
+
+use multiqueue::{BroadcastReceiver, BroadcastSender};
+
+use bitcrust_net::{BitcoinNetworkConnection, ClientMessage, NetAddr, Message, AddrMessage, Services,
+                   VersionMessage};
+
+#[cfg(test)]
+mod tests {}
+
+/// Peer bootstrapping follows the following sequence:
+/// New client == -->
+/// Remote peer == <--
+///
+/// --> Version
+/// <-- Version
+/// --> Verack
+///
+/// After the handshake, other communication can occur
+pub struct Peer {
+    host: String,
+    network_connection: BitcoinNetworkConnection,
+    send_compact: bool,
+    send_headers: bool,
+    version_sent: bool,
+    acked: bool,
+    addrs: HashSet<NetAddr>,
+    version: Option<VersionMessage>,
+    sender: BroadcastSender<ClientMessage>,
+    receiver: BroadcastReceiver<ClientMessage>,
+    inbound_messages: usize,
+    bad_messages: usize,
+    last_read: Instant,
+    thread_speed: Duration,
+}
+
+impl Debug for Peer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+
+        write!(f,
+               r"Peer {{
+    host: {},
+    send_compact: {},
+    send_headers: {},
+    version _sent: {},
+    acked: {},
+    addrs: Vec<NetAddr>,
+    version: {:?}}}",
+               self.host,
+               self.send_compact,
+               self.send_headers,
+               self.version_sent,
+               self.acked,
+               self.version)
+    }
+}
+
+impl Peer {
+    pub fn new<T: Into<String>>(host: T,
+                                sender: &BroadcastSender<ClientMessage>,
+                                receiver: &BroadcastReceiver<ClientMessage>)
+                                -> Result<Peer, Error> {
+        Peer::new_with_addrs(host, HashSet::with_capacity(1000), sender, receiver)
+    }
+
+    pub fn new_with_addrs<T: Into<String>>(host: T,
+                                           addrs: HashSet<NetAddr>,
+                                           sender: &BroadcastSender<ClientMessage>,
+                                           receiver: &BroadcastReceiver<ClientMessage>)
+                                           -> Result<Peer, Error> {
+        let host = host.into();
+        let connection = BitcoinNetworkConnection::new(host.clone())?;
+        Ok(Peer {
+            host: host,
+            network_connection: connection,
+            send_compact: false,
+            send_headers: false,
+            version_sent: false,
+            acked: false,
+            addrs: addrs,
+            version: None,
+            sender: sender.clone(),
+            receiver: receiver.clone(),
+            inbound_messages: 0,
+            bad_messages: 0,
+            last_read: Instant::now(),
+            thread_speed: Duration::from_millis(250),
+        })
+    }
+
+    fn handle_message(&mut self, message: Message) {
+        self.inbound_messages += 1;
+        if self.version.is_none() {
+            match message {
+                Message::Version(_) => {}
+                _ => {
+                    debug!("Received {:?} prior to VERSION", message);
+                    return;
+                }
+            }
+        }
+        match message {
+            Message::Version(v) => {
+                match self.version {
+                    None => {
+                        self.version = Some(v);
+                        if self.version_sent {
+                            let _ = self.send(Message::Verack);
+                            if self.addrs.len() < 100 {
+                                let _ = self.send(Message::GetAddr);
+                            }
+                        } else {
+                            let _ = self.send(Peer::version());
+                        }
+                    }
+                    Some(_) => {
+                        let _ = self.send(Peer::version());
+                    }
+                }
+            }
+            Message::Ping(nonce) => {
+                debug!("[{}] Ping", self.host);
+                let _ = self.send(Message::Pong(nonce));
+            }
+            Message::SendCompact(msg) => {
+                self.send_compact = msg.send_compact;
+            }
+            Message::Addr(addrs) => {
+                let _ = self.sender.try_send(ClientMessage::Addrs(addrs.addrs.clone()));
+                // Ensure that we don't realocate repeatedly in here
+                self.addrs.reserve(addrs.addrs.len());
+                for addr in addrs.addrs {
+                    self.addrs.insert(addr);
+                }
+            }
+            Message::GetAddr => {
+                let msg = AddrMessage { addrs: self.addrs.iter().cloned().collect() };
+                let _ = self.send(Message::Addr(msg));
+            }
+            Message::SendHeaders => {
+                self.send_headers = true;
+            }
+            Message::GetHeaders(_msg) => {}
+            Message::Verack => {
+                self.acked = true;
+            }
+            Message::Inv(_inv) => {}
+            Message::Unparsed(name, message) => {
+                // Support for alert messages has been removed from bitcoin core in March 2016.
+                // Read more at https://github.com/bitcoin/bitcoin/pull/7692
+                if name != "alert" {
+                    debug!("{} : Not handling {} yet ({:?})",
+                           self.host,
+                           name,
+                           to_hex_string(&message))
+                }
+            }
+            _ => {
+                debug!("Not handling {:?} yet", message);
+            }
+        };
+    }
+
+    pub fn run(mut self) -> thread::JoinHandle<()> {
+        thread::spawn(move || {
+            let mut last_cleanup = Instant::now();
+            let _ = self.send(Peer::version()).unwrap();
+            self.version_sent = true;
+            loop {
+                // Handle messages coming in from the network
+                self.handle_remote_peer_messages();
+                // Handle messages from other local peer connections
+                self.handle_local_peer_message();
+                // check if this is a bad peer
+                if last_cleanup.elapsed() > self.thread_speed * 5 {
+                    last_cleanup = Instant::now();
+                    self.inbound_messages = match self.inbound_messages.checked_sub(1) {
+                        Some(u) => u,
+                        None => self.inbound_messages,
+                    };
+                    self.bad_messages = match self.bad_messages.checked_sub(2) {
+                        Some(u) => u,
+                        None => self.bad_messages,
+                    };
+                    if self.inbound_messages > 0 {
+                        if self.bad_messages >= self.inbound_messages * 2 {
+                            warn!("{} sent us {} requests, and {} bad ones",
+                                  self.host,
+                                  self.inbound_messages,
+                                  self.bad_messages);
+                            break;
+                        }
+                    }
+                }
+            }
+            let _ = self.sender.try_send(ClientMessage::Closing(self.host));
+        })
+
+    }
+
+    fn handle_remote_peer_messages(&mut self) {
+        if self.last_read.elapsed() < self.thread_speed {
+            let time = self.thread_speed - self.last_read.elapsed();
+            thread::sleep(time);
+        }
+        self.last_read = Instant::now();
+        if let Some(msg) = self.network_connection.try_recv() {
+            match msg {
+                Ok(msg) => self.handle_message(msg),
+                Err(()) => self.bad_messages += 1,
+            }
+
+        }
+    }
+
+    fn handle_local_peer_message(&mut self) {
+        if let Ok(msg) = self.receiver.try_recv() {
+            match msg {
+                ClientMessage::Addrs(addrs) => {
+                    // We only want to send `Addr`s that we don't think the remote
+                    // peer knows about already
+                    let mut addrs_to_send = Vec::with_capacity(addrs.len());
+                    for addr in addrs {
+                        if !self.addrs.insert(addr.clone()) {
+                            addrs_to_send.push(addr);
+                        }
+                    }
+                    let _ = self.send(Message::Addr(AddrMessage { addrs: addrs_to_send }));
+                }
+                ClientMessage::Closing(_) => {}
+                // _ => info!("Ignoring msg: {:?}", msg),
+            }
+        }
+    }
+
+    fn send(&mut self, message: Message) -> Result<(), Error> {
+        self.network_connection.try_send(message)
+    }
+
+    fn version() -> Message {
+        Message::Version(VersionMessage {
+            version: 70015,
+            services: Services::from(1),
+            timestamp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64,
+            addr_recv: Peer::addr(Ipv6Addr::from_str("::ffff:127.0.0.1").unwrap(), 8333, None),
+            addr_send: Peer::addr(Ipv6Addr::from_str("::ffff:127.0.0.1").unwrap(), 8333, None),
+            nonce: 1,
+            user_agent: "/bitcrust:0.1.0/".into(),
+            start_height: 0,
+            relay: false,
+        })
+    }
+
+    fn addr(ip: Ipv6Addr, port: u16, time: Option<u32>) -> NetAddr {
+        NetAddr {
+            time: time,
+            services: Services::from(1),
+            ip: ip,
+            port: port,
+        }
+    }
+}
+
+fn to_hex_string(bytes: &[u8]) -> String {
+    let strs: Vec<String> = bytes.iter()
+        .map(|b| format!("{:02X}", b))
+        .collect();
+    strs.join(" ")
+}
---
+++ bitcrustd/src/peer_manager.rs
@@ -0,0 +1,234 @@
+use std::collections::HashSet;
+use std::env::home_dir;
+use std::net::Ipv6Addr;
+use std::str::FromStr;
+use std::thread;
+
+use multiqueue::{BroadcastReceiver, BroadcastSender, broadcast_queue};
+use rusqlite::{Error, Connection};
+
+use bitcrust_net::{ClientMessage, NetAddr, Services};
+use peer::Peer;
+
+pub struct PeerManager {
+    addrs: HashSet<NetAddr>,
+    database: Connection,
+    sender: BroadcastSender<ClientMessage>,
+    receiver: BroadcastReceiver<ClientMessage>,
+    peers: Vec<(String, thread::JoinHandle<()>)>,
+}
+
+//pub time: Option<u32>,
+// pub services: u64,
+// pub ip: Ipv6Addr,
+// pub port: u16,
+
+impl PeerManager {
+    pub fn new() -> PeerManager {
+        let mut path = home_dir().expect("Can't figure out where your $HOME is");
+        path.push(".bitcrust.dat");
+        debug!("Connecting to DB at {:?}", path);
+        let db = Connection::open(path).expect("Couldn't open SQLite connection");
+        debug!("Have a database connection");
+        db.execute("
+             CREATE TABLE IF NOT EXISTS peers (
+                 time \
+                      INTEGER,
+                 services INTEGER,
+                 ip TEXT,
+                 \
+                      port INTEGER
+             );",
+                     &[])
+            .unwrap();
+
+        let addrs: HashSet<NetAddr> = {
+            let mut stmt =
+                db.prepare("SELECT time, services, ip, port from peers ORDER BY time DESC \
+                              limit 1000")
+                    .unwrap();
+
+            let addrs: HashSet<NetAddr> = stmt.query_map(&[], |row| {
+                    NetAddr {
+                        time: Some(row.get(0)),
+                        services: Services::from(row.get::<_, i64>(1) as u64),
+                        ip: Ipv6Addr::from_str(&row.get::<_, String>(2))
+                            .unwrap_or(Ipv6Addr::from_str("::").unwrap()),
+                        port: row.get(3),
+                    }
+                })
+                .unwrap()
+                .filter_map(|l| l.ok())
+                .collect();
+            addrs
+        };
+        info!("Pulled {} addrs from the database", addrs.len());
+
+        let (sender, receiver) = broadcast_queue(200);
+
+        debug!("Setup peers");
+
+        PeerManager {
+            database: db,
+            addrs: addrs,
+            receiver: receiver,
+            sender: sender,
+            peers: Vec::with_capacity(100),
+        }
+    }
+
+    pub fn execute(&mut self) -> ! {
+        debug!("Executing!");
+        self.initialize_peers();
+        // self.peers.append(&mut peers);
+        // println!("About to make a scope for a peer");
+        // debug!("Made a crossbeam scope!");
+
+        // scope.spawn(|| {
+        // debug!("Spawning peer thread");
+        // });
+        // });
+        loop {
+            info!("Currently connected to {}/{} peers",
+                  self.peers.len(),
+                  self.addrs.len());
+            match self.receiver.recv() {
+                Ok(s) => {
+                    match s {
+                        ClientMessage::Addrs(addrs) => self.addr_message(addrs),
+                        ClientMessage::Closing(hostname) => {
+                            self.peers
+                                .retain(|ref peer| peer.0 != hostname);
+                        }
+                    }
+                }
+                Err(e) => {
+                    trace!("Some error with thread communication, {:?}", e)
+                    // match e {
+                    //     Empty => {}
+                    //     _ => trace!("Some error with thread communication, {:?}", e),
+                    // }
+                }
+            }
+            self.initialize_peers();
+        }
+    }
+
+    fn addr_message(&mut self, addrs: Vec<NetAddr>) {
+        info!("Peer sent us {} new addrs", addrs.len());
+        for addr in addrs.into_iter() {
+            match self.add_addr(addr) {
+                Ok(_) => {}
+                Err(e) => warn!("Error adding addr: {:?}", e),
+            }
+        }
+    }
+
+    fn initialize_peers(&mut self) {
+        if self.peers.len() >= 100 {
+            return;
+        }
+
+        if self.addrs.len() > 0 {
+            let connected_addrs: Vec<String> = self.peers.iter().map(|t| t.0.clone()).collect();
+            for (addr, host) in self.addrs
+                .iter()
+                .map(|addr| {
+                    let s = format!("{}:{}", addr.ip, addr.port);
+                    (addr, s)
+                })
+                .filter(|a| !connected_addrs.contains(&a.1)) {
+                if self.peers.len() >= 100 {
+                    break;
+                }
+                match Peer::new(&host[..], &self.sender, &self.receiver) {
+                    Ok(peer) => {
+                        let _ = self.update_time(addr);
+                        self.peers.push((host.to_string(), peer.run()));
+                    }
+                    Err(e) => {
+                        debug!("Failed to connect to peer at {} :: {:?}", addr.ip, e);
+                        let r = self.remove_addr(&addr);
+                        debug!("Removing addr res: {:?}", r);
+                    }
+                };
+            }
+        }
+
+        if self.peers.len() == 0 {
+            for hostname in ["seed.bitcoin.sipa.be:8333",
+                             "dnsseed.bluematt.me:8333",
+                             "dnsseed.bitcoin.dashjr.org:8333",
+                             "seed.bitcoinstats.com:8333",
+                             // "seed.bitcoin.jonasschnelli.ch:8333",
+                             // "seed.btc.petertodd.org:8333"
+                             ]
+                .iter() {
+                // info!("Trying to connect")
+                match Peer::new(*hostname, &self.sender, &self.receiver) {
+                    Ok(peer) => {
+                        self.peers.push((hostname.to_string(), peer.run()));
+                    }
+                    Err(e) => warn!("Error connecting to {}: {:?}", hostname, e),
+                }
+
+            }
+        }
+    }
+
+    fn add_addr(&mut self, addr: NetAddr) -> Result<(), Error> {
+        if self.update_time(&addr).is_some() {
+            let updating: Vec<NetAddr> = self.addrs
+                .iter()
+                .filter(|a| a.ip == addr.ip && a.port == addr.port)
+                .map(|a| a.clone())
+                .collect();
+            for mut a in updating {
+                self.addrs.remove(&a);
+                a.time = addr.time;
+                self.addrs.insert(a);
+            }
+            return Ok::<(), Error>(());
+        }
+        let mut stmt = self.database
+            .prepare("INSERT INTO peers (time, services, ip, port) VALUES (?, ?, ?, ?)")?;
+        stmt.execute(&[&format!("{}", addr.time.unwrap_or(0)),
+                       &format!("{}", addr.services.encode() as i64),
+                       &format!("{}", addr.ip),
+                       &format!("{}", addr.port)])?;
+        self.addrs.insert(addr.clone());
+        Ok(())
+    }
+
+    fn remove_addr(&self, addr: &NetAddr) -> Result<(), Error> {
+        // self.addrs.retain(|&ref a| !(a.ip == addr.ip && a.port == addr.port));
+        let mut stmt = self.database
+            .prepare("DELETE FROM peers WHERE ip = ? AND port = ?")?;
+        stmt.execute(&[&format!("{}", addr.ip), &format!("{}", addr.port)])?;
+        Ok(())
+    }
+
+    fn update_time(&self, addr: &NetAddr) -> Option<()> {
+        let mut update = match self.database
+            .prepare("UPDATE peers SET time = ? WHERE ip = ? AND port = ?") {
+            Ok(s) => s,
+            Err(e) => {
+                warn!("Couldn't prepare peer: {:?}", e);
+                return None;
+            }
+        };
+        let count = match update.execute(&[&format!("{}", addr.time.unwrap_or(0)),
+                                           &format!("{}", addr.ip),
+                                           &format!("{}", addr.port)]) {
+            Ok(s) => s,
+            Err(e) => {
+                warn!("Couldn't update addr's time: {:?}", e);
+                return None;
+            }
+        };
+        if count > 0 {
+            return Some(());
+        }
+        None
+    }
+}
